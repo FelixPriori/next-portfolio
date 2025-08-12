00ab74b5b198445cfe1c0f16a0e02dac7393db56
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2025-08-12 15:33:26 -0400

    adds some metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next'
 import AboutSection from './_sections/AboutSection'
 import ContactSection from './_sections/ContactSection'
 import IntroSection from './_sections/IntroSection'
@@ -6,6 +7,11 @@ import TestimonialsSection from './_sections/TestimonialsSection'
 import TrainingSection from './_sections/TrainingSection'
 import WorkSection from './_sections/WorkSection'
 import styles from './layout.module.scss'
+
+export const metadata: Metadata = {
+	title: "Felix's Porftolio",
+	description: 'Felix Rioux Sabourin - Professional Web Developer',
+}
 
 export default function Home() {
 	return (
