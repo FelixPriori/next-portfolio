bc639497bfe7ae3884474f326f66dad274c3f08a
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2025-08-16 16:21:46 -0400

    updates about section

--- src/app/_sections/AboutSection.tsx
+++ src/app/_sections/AboutSection.tsx
@@ -7,18 +7,24 @@ export default function AboutSection() {
 				<h2 className={styles.subtitle}>About Me</h2>
 			</div>
 			<Paragraph>
-				With over <Mark>four years of experience</Mark> in front-end
+				With over <Mark>five years of experience</Mark> in front-end
 				development, I am a dynamic professional and an arts-oriented individual
 				captivated by the transformative power of programming to bring ideas to
-				life. I have contributed to <Mark>over a dozen client projects</Mark>{' '}
-				across a <Mark>wide range of industries</Mark>, and I always have a
-				passion project ongoing.
+				life. The first four years of my career were spent working for agencies,
+				and I have contributed to <Mark>over a dozen client projects</Mark>{' '}
+				across a <Mark>wide range of industries</Mark>.
 			</Paragraph>
 			<Paragraph>
-				I am passionate about coding and always seek new ways to improve my
-				craft. I love to work in <Mark>tight-knit teams</Mark> and find extreme
-				satisfaction in{' '}
-				<Mark>finding a great solution to a challenging problem</Mark>.
+				Since November of 2024, I have been working full time to build an{' '}
+				<Mark>in-house SAAS</Mark> as a{' '}
+				<Mark>consultant web development engineer</Mark>, using my expertise to
+				create a one of a kind service in the field of supply chains.
+			</Paragraph>
+			<Paragraph>
+				I thrive most while working in <Mark>tight-knit teams</Mark> and I my
+				biggest satisfaction is to{' '}
+				<Mark>find a great solution to a challenging problem</Mark>. Being in a
+				state of flow while coding is what I love most about being a dev.
 			</Paragraph>
 		</section>
 	)
